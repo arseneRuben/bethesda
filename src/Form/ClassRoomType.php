d65146cd2ce1c40ea89a430e8941882a6c8f032f
Arsene Fokam P <2210100@isi-montreal.com>
2023-07-30 17:29:39 -0400

    ajout de la colonne du resultat aux examens officiel dans la page de presentation d'un eleve

--- src/Form/ClassRoomType.php
+++ src/Form/ClassRoomType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ClassRoom;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +14,9 @@ class ClassRoomType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('apc')
+            ->add('apc', CheckboxType::class, array('label' => 'Classe d\'examen ?'))
             ->add('level')
-            ->add('fullTeacher')
-        ;
+            ->add('fullTeacher');
     }
 
     public function configureOptions(OptionsResolver $resolver)
