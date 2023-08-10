24d1561f624d3005522849fd12456bb9206c3d23
Arsene Fokam P <2210100@isi-montreal.com>
2023-08-10 10:56:11 -0400

    resultats des examens officieles de PC au frontEnd

--- src/Controller/SchoolController.php
+++ src/Controller/SchoolController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use App\Repository\ClassRoomRepository;
 use App\Repository\SchoolYearRepository;
+use App\Repository\SubscriptionRepository;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,21 +29,77 @@ class SchoolController extends AbstractController
     private $userRepo;
     private $rmRepo;
     private $scRepo;
+    private $subRepo;
 
-    public function __construct(EntityManagerInterface $em, UserRepository $userRepo, SchoolYearRepository $scRepo, ClassRoomRepository $rmRepo)
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepo, SchoolYearRepository $scRepo, ClassRoomRepository $rmRepo, SubscriptionRepository $subRepo)
     {
         $this->em = $em;
         $this->userRepo = $userRepo;
         $this->scRepo = $scRepo;
         $this->rmRepo = $rmRepo;
+        $this->subRepo = $subRepo;
     }
     /**
      * @Route("/", name="app_home")
      */
     public function index(): Response
     {
+        $rooms = $this->rmRepo->findBy(array("apc" => true));
+        $year = $this->scRepo->findOneBy(array("activated" => true));
 
-        return $this->render('school/index.html.twig');
+        $results = [];
+
+        foreach ($rooms as $room) {
+
+            $officialExamResults = $this->subRepo->countByMention($year, $room);
+            $mentionCategories = [];
+            $mentionCountCategories = [];
+            foreach ($officialExamResults as $exam) {
+                switch ($exam["officialExamResult"]) {
+                    case  "0":
+                        $mentionCategories[] = "ECHEC";
+                        break;
+                    case  "1p":
+                        $mentionCategories[] = "PASSABLE";
+                        break;
+                    case  "1a":
+                        $mentionCategories[] = "ASSEZ-BIEN";
+                        break;
+                    case  "1b":
+                        $mentionCategories[] = "BIEN";
+                        break;
+                    case  "1t":
+                        $mentionCategories[] = "TRES-BIEN";
+                        break;
+                    case  "1e":
+                        $mentionCategories[] = "EXCELLENT";
+                        break;
+                    case  "A":
+                        $mentionCategories[] = "5 POINTS";
+                        break;
+                    case  "B":
+                        $mentionCategories[] = "4 POINTS";
+                        break;
+                    case  "C":
+                        $mentionCategories[] = "3 POINTS";
+                        break;
+                    case  "D":
+                        $mentionCategories[] = "2 POINTS";
+                        break;
+                    case  "E":
+                        $mentionCategories[] = "1 POINT";
+                        break;
+                }
+                $mentionCountCategories[] = $exam["count"];
+            }
+            $couple["mentionCategories"] = $mentionCategories;
+            $couple["mentionCountCategories"] = $mentionCountCategories;
+            $results[str_replace(' ', '', strtolower($room->getName()))] = json_encode($couple);
+        }
+
+        // dd($results);
+
+        return $this->render('school/index.html.twig', compact("results"));
     }
 
 
