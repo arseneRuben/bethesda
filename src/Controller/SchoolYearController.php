5544fcc965a94e950f56293952f00b7959e62b5b
Arsene Fokam P <2210100@isi-montreal.com>
2023-08-15 19:35:59 -0400

    fake update

--- src/Controller/SchoolYearController.php
+++ src/Controller/SchoolYearController.php
@@ -51,78 +51,63 @@ class SchoolYearController extends AbstractController
      * @Template()
      */
 
-     public function showAction(SchoolYear $school_year, SchoolYearRepository $schoolYearRepository): Response
-     {   
-         $em = $this->getDoctrine()->getManager();
-         
-         $allSchoolYears = $schoolYearRepository->findAllActivatedExcept($school_year);
-         
-         foreach ($allSchoolYears as $otherSchoolYear) {
-             $otherSchoolYear->setActivated(false);
-             $em->persist($otherSchoolYear);
-         }
-         
-         if (!$school_year->getActivated()) {
-             $school_year->setActivated(true);
-             $em->persist($school_year);
-         }
- 
-         $em->flush();
-         
-         if ($school_year->getActivated()) {
-             foreach ($school_year->getSubscriptions() as $sub) {
-                 if ($sub->getStudent() !== null) {
-                     var_dump($sub);
-                     // $sub->getStudent()->setEnrolled(true);
-                 }
-             }
-         }
-         
-         return $this->render('school_year/show.html.twig', compact("school_year"));
-     }
- 
+    public function showAction(SchoolYear $school_year, SchoolYearRepository $schoolYearRepository): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $allSchoolYears = $schoolYearRepository->findAllActivatedExcept($school_year);
+
+        foreach ($allSchoolYears as $otherSchoolYear) {
+            $otherSchoolYear->setActivated(false);
+            $em->persist($otherSchoolYear);
+        }
+
+        if (!$school_year->getActivated()) {
+            $school_year->setActivated(true);
+            $em->persist($school_year);
+        }
+
+        $em->flush();
+
+        if ($school_year->getActivated()) {
+            foreach ($school_year->getSubscriptions() as $sub) {
+                if ($sub->getStudent() !== null) {
+                    var_dump($sub);
+                    // $sub->getStudent()->setEnrolled(true);
+                }
+            }
+        }
+
+        return $this->render('school_year/show.html.twig', compact("school_year"));
+    }
+
     /**
      * @Route("/create",name= "admin_schoolyears_new", methods={"GET","POST"})
      */
-    public function create(Request $request, SchoolYearRepository $schoolYearRepository): Response
+    public function create(Request $request): Response
     {
         if (!$this->getUser()) {
-            $this->addFlash('warning', 'You need to log in first!');
+            $this->addFlash('warning', 'You need login first!');
             return $this->redirectToRoute('app_login');
         }
-        
         if (!$this->getUser()->isVerified()) {
             $this->addFlash('warning', 'You need to have a verified account!');
             return $this->redirectToRoute('app_login');
         }
-        
-        $em = $this->getDoctrine()->getManager();
         $schoolyear = new SchoolYear();
         $form = $this->createForm(SchoolYearType::class, $schoolyear);
         $form->handleRequest($request);
-        
         if ($form->isSubmitted() && $form->isValid()) {
-            $allSchoolYears = $schoolYearRepository->findAllActivatedExcept($schoolyear);
-            
-            foreach ($allSchoolYears as $otherSchoolYear) {
-                $otherSchoolYear->setActivated(false);
-                $em->persist($otherSchoolYear);
-            }
-            
-            $schoolyear->setActivated(true);
-            $em->persist($schoolyear);
-            $em->flush();
-            
-            $this->addFlash('success', 'SchoolYear successfully created');
+            $this->em->persist($schoolyear);
+            $this->em->flush();
+            $this->addFlash('success', 'SchoolYear succesfully created');
             return $this->redirectToRoute('admin_school_years');
         }
-        
         return $this->render(
             'school_year/new.html.twig',
             ['form' => $form->createView()]
         );
     }
-
 
     /**
      * Displays a form to edit an existing SchoolYearme entity.
@@ -130,34 +115,19 @@ class SchoolYearController extends AbstractController
      * @Route("/{id}/edt", name="admin_schoolyears_edit", requirements={"id"="\d+"}, methods={"GET","PUT"})
      * @Template()
      */
-    public function edit(Request $request, SchoolYear $schoolyear, SchoolYearRepository $schoolYearRepository): Response
+    public function edit(Request $request, SchoolYear $schoolyear): Response
     {
-        $em = $this->getDoctrine()->getManager();
-        
+
         $form = $this->createForm(SchoolYearType::class, $schoolyear, [
             'method' => 'GET',
         ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $allSchoolYears = $schoolYearRepository->findAllActivatedExcept($schoolyear);
-            
-            foreach ($allSchoolYears as $otherSchoolYear) {
-                $otherSchoolYear->setActivated(false);
-                $em->persist($otherSchoolYear);
-            }
-            
-            if (!$schoolyear->getActivated()) {
-                $schoolyear->setActivated(true);
-                $em->persist($schoolyear);
-            }
-
-            $em->flush();
-            
-            $this->addFlash('success', 'SchoolYear successfully updated');
+            $this->em->flush();
+            $this->addFlash('success', 'SchoolYear succesfully updated');
             return $this->redirectToRoute('admin_school_years');
         }
-        
         return $this->render('school_year/edit.html.twig', [
             'schoolyear' => $schoolyear,
             'form' => $form->createView()
