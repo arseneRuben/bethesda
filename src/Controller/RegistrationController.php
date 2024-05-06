e844789687fe16e03f14967f56650173dc5e688b
arsene <fopoar@gmail.com>
2024-05-06 04:36:26 -0300

    retrait de l'envoie demail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,8 +55,9 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation(
+           /* $this->emailVerifier->sendEmailConfirmation(
                 'app_verify_email',
                 $user,
                 (new TemplatedEmail())
@@ -64,7 +65,7 @@ class RegistrationController extends AbstractController
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('emails/registration/confirmation_email.html.twig')
-            );
+            ); */
             // do anything else you need here, like send an email
 
             return $guardHandler->authenticateUserAndHandleSuccess(
